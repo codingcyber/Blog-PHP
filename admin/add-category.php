1c62e3a6e2952ff035825eee5d04c1ea8d45fe84
codingcyber <vivek@codingcyber.com>
2019-07-10 16:57:40 +0530

    4-1 Add Category

--- admin/add-category.php
+++ admin/add-category.php
@@ -1,5 +1,56 @@
-<?php include('includes/header.php'); ?>
-<?php include('includes/navigation.php'); ?>
+<?php 
+include('includes/check-login.php');
+require_once('../includes/connect.php'); 
+if(isset($_POST) & !empty($_POST)){
+    // PHP Form Validations
+    if(empty($_POST['title'])){$errors[] = "Title Field is Required";}
+    // CSRF Token Validation
+    if(isset($_POST['csrf_token'])){
+        if($_POST['csrf_token'] === $_SESSION['csrf_token']){
+        }else{
+            $errors[] = "Problem with CSRF Token Verification";
+        }
+    }else{
+        $errors[] = "Problem with CSRF Token Validation";
+    }
+
+    // CSRF Token Time Validation
+    $max_time = 60*60*24;
+    if(isset($_SESSION['csrf_token_time'])){
+        $token_time = $_SESSION['csrf_token_time'];
+        if(($token_time + $max_time) >= time()){
+        }else{
+            $errors[] = "CSRF Token Expired";
+            unset($_SESSION['csrf_token']);
+            unset($_SESSION['csrf_token_time']);
+        }
+    }else{
+        unset($_SESSION['csrf_token']);
+        unset($_SESSION['csrf_token_time']);
+    }
+    if(empty($errors)){
+        $sql = "INSERT INTO categories (title, description, slug) VALUES (:title, :description, :slug)";
+        $result = $db->prepare($sql);
+        $values = array(':title'            => $_POST['title'],
+                        ':description'      => $_POST['description'],
+                        ':slug'             => $_POST['slug']
+                        );
+        $res = $result->execute($values);
+        if($res){
+            header('location:view-categories.php');
+        }else{
+            $errors[] = "Failed to Add Category";
+        }
+    }
+}
+// Create CSRF token
+$token = md5(uniqid(rand(), TRUE));
+$_SESSION['csrf_token'] = $token;
+$_SESSION['csrf_token_time'] = time();
+
+include('includes/header.php');
+include('includes/navigation.php');
+?>
 <div id="page-wrapper" style="min-height: 345px;">
     <div class="row">
         <div class="col-lg-12">
@@ -15,28 +66,42 @@
                     Create a New Category Here...
                 </div>
                 <div class="panel-body">
+                    <?php
+                        if(!empty($messages)){
+                            echo "<div class='alert alert-success'>";
+                            foreach ($messages as $message) {
+                                echo "<span class='glyphicon glyphicon-ok'></span>&nbsp;". $message ."<br>";
+                            }
+                            echo "</div>";
+                        }
+                    ?>
+                    <?php
+                        if(!empty($errors)){
+                            echo "<div class='alert alert-danger'>";
+                            foreach ($errors as $error) {
+                                echo "<span class='glyphicon glyphicon-remove'></span>&nbsp;". $error ."<br>";
+                            }
+                            echo "</div>";
+                        }
+                    ?>
                     <div class="row">
                         <div class="col-lg-12">
-                            <form role="form">
+                            <form role="form" method="post">
+                                <input type="hidden" name="csrf_token" value="<?php echo $token; ?>">
                                 <div class="form-group">
                                     <label>Category Title</label>
-                                    <input class="form-control" placeholder="Enter Article Title">
+                                    <input class="form-control" name="title" placeholder="Enter Article Title">
                                 </div>
                                 <div class="form-group">
-                                    <label>Article Description</label>
-                                    <textarea class="form-control" rows="3"></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label>Category Image</label>
-                                    <input type="file">
+                                    <label>Category Description</label>
+                                    <textarea class="form-control" name="description" rows="3"></textarea>
                                 </div>
                                 <div class="form-group">
                                     <label>Category Slug</label>
-                                    <input class="form-control" placeholder="Enter Category Slug Here">
+                                    <input class="form-control" name="slug" placeholder="Enter Category Slug Here">
                                 </div>
 
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                                <button type="reset" class="btn btn-danger">Reset </button>
+                                <input type="submit" class="btn btn-success" value="Submit" />
                             </form>
                         </div>
                         <!-- /.col-lg-6 (nested) -->   
