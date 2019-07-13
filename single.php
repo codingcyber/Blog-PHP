ceb09e5423d1bc964794a4649e1ba8fd347d595d
codingcyber <vivek@codingcyber.com>
2019-07-13 21:49:59 +0530

    9-4 Blog Single Article Page

--- single.php
+++ single.php
@@ -1,5 +1,17 @@
-<?php include('includes/header.php'); ?>
-<?php include('includes/navigation.php'); ?>
+<?php 
+require_once('includes/connect.php');
+include('includes/header.php');
+include('includes/navigation.php'); 
+$sql = "SELECT * FROM posts WHERE id=?";
+$result = $db->prepare($sql);
+$result->execute(array($_GET['id']));
+$post = $result->fetch(PDO::FETCH_ASSOC);
+
+$usersql = "SELECT * FROM users WHERE id=?";
+$userresult = $db->prepare($usersql);
+$userresult->execute(array($post['uid']));
+$user = $userresult->fetch(PDO::FETCH_ASSOC);
+?>
 <!-- Page Content -->
 <div class="container">
 
@@ -9,43 +21,33 @@
     <div class="col-lg-8">
 
       <!-- Title -->
-      <h1 class="mt-4">Post Title</h1>
+      <h1 class="mt-4"><?php echo $post['title']; ?></h1>
 
       <!-- Author -->
       <p class="lead">
         by
-        <a href="#">Start Bootstrap</a>
+        <a href="user-posts.php?id=<?php echo $user['id']; ?>"><?php if((isset($user['fname']) || isset($user['lname'])) & (!empty($user['fname']) || !empty($user['lname']))) {echo $user['fname'] . " " . $user['lname']; }else{echo $user['username']; } ?></a>
       </p>
 
       <hr>
 
       <!-- Date/Time -->
-      <p>Posted on January 1, 2018 at 12:00 PM</p>
+      <p>Posted on <?php echo $post['created']; ?></p>
 
       <hr>
 
       <!-- Preview Image -->
-      <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
-
+      <?php if(isset($post['pic']) & !empty($post['pic'])){ ?>
+          <img class="img-fluid rounded" src="<?php echo $post['pic']; ?>" alt="">
+      <?php }else{ ?>
+          <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
+      <?php } ?>
       <hr>
 
       <!-- Post Content -->
-      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-
-      <blockquote class="blockquote">
-        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-        <footer class="blockquote-footer">Someone famous in
-          <cite title="Source Title">Source Title</cite>
-        </footer>
-      </blockquote>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
+      <div class="content">
+        <?php echo $post['content']; ?>
+      </div>
 
       <hr>
 
