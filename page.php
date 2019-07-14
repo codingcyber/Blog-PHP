b5f21cbf0345f0c0cf9e3ea99a0faf685aee38b8
codingcyber <vivek@codingcyber.com>
2019-07-14 14:09:12 +0530

    9-9 Blog Page

---
+++ page.php
@@ -0,0 +1,111 @@
+<?php 
+require_once('includes/connect.php');
+include('includes/header.php');
+include('includes/navigation.php'); 
+$sql = "SELECT * FROM pages WHERE id=?";
+$result = $db->prepare($sql);
+$result->execute(array($_GET['id']));
+$page = $result->fetch(PDO::FETCH_ASSOC);
+
+$usersql = "SELECT * FROM users WHERE id=?";
+$userresult = $db->prepare($usersql);
+$userresult->execute(array($page['uid']));
+$user = $userresult->fetch(PDO::FETCH_ASSOC);
+?>
+<!-- Page Content -->
+<div class="container">
+
+  <div class="row">
+
+    <!-- Post Content Column -->
+    <div class="col-lg-8">
+
+      <!-- Title -->
+      <h1 class="mt-4"><?php echo $page['title']; ?></h1>
+
+      <!-- Author -->
+      <p class="lead">
+        by
+        <a href="user-posts.php?id=<?php echo $user['id']; ?>"><?php if((isset($user['fname']) || isset($user['lname'])) & (!empty($user['fname']) || !empty($user['lname']))) {echo $user['fname'] . " " . $user['lname']; }else{echo $user['username']; } ?></a>
+      </p>
+
+      <hr>
+
+      <!-- Date/Time -->
+      <p>Posted on <?php echo $page['created']; ?></p>
+
+      <hr>
+
+      <!-- Preview Image -->
+      <?php if(isset($page['pic']) & !empty($page['pic'])){ ?>
+          <img class="img-fluid rounded" src="<?php echo $page['pic']; ?>" alt="">
+      <?php }else{ ?>
+          <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
+      <?php } ?>
+      <hr>
+
+      <!-- Post Content -->
+      <div class="content">
+        <?php echo $page['content']; ?>
+      </div>
+
+      <hr>
+
+      <!-- Comments Form -->
+      <div class="card my-4">
+        <h5 class="card-header">Leave a Comment:</h5>
+        <div class="card-body">
+          <form>
+            <div class="form-group">
+              <textarea class="form-control" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+
+      <!-- Single Comment -->
+      <div class="media mb-4">
+        <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+        <div class="media-body">
+          <h5 class="mt-0">Commenter Name</h5>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+      </div>
+
+      <!-- Comment with nested comments -->
+      <div class="media mb-4">
+        <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+        <div class="media-body">
+          <h5 class="mt-0">Commenter Name</h5>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+
+          <div class="media mt-4">
+            <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+            <div class="media-body">
+              <h5 class="mt-0">Commenter Name</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </div>
+
+          <div class="media mt-4">
+            <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+            <div class="media-body">
+              <h5 class="mt-0">Commenter Name</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+    <?php include('includes/sidebar.php'); ?>
+
+  </div>
+  <!-- /.row -->
+
+</div>
+<!-- /.container -->
+<?php include('includes/footer.php'); ?>
